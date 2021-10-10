841007271031e72e9373948c94a513559d1c15e5
jessinicz <jessinicki77@gmail.com>
2021-10-10 18:15:03 -0500

    Check winner and update status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,12 @@ window.addEventListener("load", () =>{
     board = Array.from(document.querySelectorAll("#board >div"));
     let change =  true;
 
+    let winnerLog = [[0,1,2],[3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8] , [2,4,6]];
+    let winner;
+    let xWins;
+    let oWins;
+
+
     board.forEach(el =>{
         el.classList.add("square");
         el.onclick = () => {
@@ -19,6 +25,30 @@ window.addEventListener("load", () =>{
                 el.innerHTML = "O";
                 change = true;
             }
+            winner = gameWinner();
+            if (winner == 1)
+            {
+                state.classList.add("you-won");
+                state.innerHTML = "Congratulations! X is the Winner!";
+                board.forEach((sqr) => {
+                    sqr.onclick = (s) =>{
+                        s.preventDefault();
+                    }
+
+                })
+            }
+            else if (winner == 0)
+            {
+                state.classList.add("you-won");
+                state.innerHTML = "Congratulations! O is the Winner!";
+                board.forEach((sqr) => {
+                    sqr.onclick = (s) =>{
+                        s.preventDefault();
+                    }
+
+                })
+            }
+
             }
     })
     el.onmouseover = () => {
