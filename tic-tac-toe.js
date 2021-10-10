e29bb513372eea4971ea731ff6b56acaa5d8f2c7
jessinicz <jessinicki77@gmail.com>
2021-10-10 18:09:04 -0500

    Add X or O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,5 +4,22 @@ window.addEventListener("load", () =>{
 
     board.forEach(el =>{
         el.classList.add("square");
+        el.onclick = () => {
+            if (el.innerHTML == "X" || el.innerHTML == "O"){
+                el.innerHTML = el.innerHTML;
+            }
+            else if (change){
+                el.classList.add("X");
+                el.innerHTML = "X";
+                change = false;
+            }
+            else if(change == false){
+
+                el.classList.add("O");
+                el.innerHTML = "O";
+                change = true;
+            }
+            }
     })
+
 });
